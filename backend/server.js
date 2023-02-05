1802d039a63e4d17a6909015638287f84b98e503
jeffmikhail <mikhailcruz78@gmail.com>
2023-02-05 21:39:45 +0800

    express setup

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const colors = require('colors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`.brightCyan.underline.italic);
+});
